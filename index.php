40468af1e90f2901da1f5540073cdbf008141af3
drielisantana <andrielimartins0308@gmail.com>
2025-02-10 20:27:48 -0300

    aula 10-02-2025

--- index.php
+++ index.php
@@ -7,6 +7,61 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+<div class="interface">
+
+  <header class="cabecalho">
+
+   <nav class="menu">
+
+   <div class="logo">
+     <img src="imagens/logo.png" alt="LOGO" width="150">
+   </div>
+
+     <ul>
+        <li>Home</li>
+        <li>Serviços</li>
+        <li>Produtos</li>
+        <Li>Portifolio</Li>
+        <li>Sobre</li>
+        <li>Contatos</li>
+        
+   </ul>
+   
+   </ol>
+
+   <ol>
+
+   </nav>
+    <div class="titles">
+
+    <h1 class="titulo">Entregando serviços de qualidade</h1>
+    <h1 class="subtitulo"> Soluções em TI</h1>
+
+    <P class= "parágrafo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. </P>
+    <div>
+        <div class= "socialIcons">
+
+        <a href="#">Twiter</a>
+        <a href="#">Facebook</a>
+        <a href="#">Linkedin</a>
+        <a href="#">Instagram</a>
+
+        
+
+
+
+    
+    
+
+        
+        ></div>
+    </div>
+
+
+  </header>
+</div>
+</header>
     
 </body>
 </html>
