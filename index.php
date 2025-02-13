d033562aaa9982f8fa3575f1cec75363cc82f328
drielisantana <andrielimartins0308@gmail.com>
2025-02-13 20:20:33 -0300

    aula 13/02/2025

--- index.php
+++ index.php
@@ -1,67 +1,85 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PORTFOLIO</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>portifólio</title>
+
+    <style>
+      *{
+        margin: 0;
+        padding: 0; 
+      
+      }
+
+      p {
+        border: 1px solid blue;
+      }
+
+
+      h1 {
+        background: pink;
+      }
+
+      body{
+        background-image: url('imagens/logo.png');
+        background-repeat: no-repeat;
+        background-position: center;
+ }
+      
+
+    </style>
+
+    <link rel="stylesheet" href="css/styles.css />
+    
 </head>
 <body>
 
-<div class="interface">
 
-  <header class="cabecalho">
+  <div class="interface">
 
-   <nav class="menu">
+    <header class="cabecalho">
 
-   <div class="logo">
-     <img src="imagens/logo.png" alt="LOGO" width="150">
-   </div>
-
-     <ul>
-        <li>Home</li>
-        <li>Serviços</li>
-        <li>Produtos</li>
-        <Li>Portifolio</Li>
-        <li>Sobre</li>
-        <li>Contatos</li>
-        
-   </ul>
-   
-   </ol>
-
-   <ol>
-
-   </nav>
-    <div class="titles">
-
-    <h1 class="titulo">Entregando serviços de qualidade</h1>
-    <h1 class="subtitulo"> Soluções em TI</h1>
-
-    <P class= "parágrafo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. </P>
-    <div>
-        <div class= "socialIcons">
-
-        <a href="#">Twiter</a>
-        <a href="#">Facebook</a>
-        <a href="#">Linkedin</a>
-        <a href="#">Instagram</a>
-
-        
+        <nav class="menu">
 
 
+            <div class="logo">
 
-    
-    
+                <img src="imagens/logo.png" alt="LOGO" width="150" />
 
-        
-        ></div>
-    </div>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Serviços</li>
+                <li>Produtos</li>
+                <li>Portifólio</li>
+                <li>Sobre</li>
+                <li>Contato</li>
+            </ul>
 
 
-  </header>
-</div>
-</header>
-    
+        </nav>
+
+        <div class="title">
+
+            <h1 class="titulo">Entregando serviços de qualidade</h1>
+            <h1 class="subtitulo">Soluçoes em TI</h1>
+
+            <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ex voluptas fugiat. Praesentium, assumenda nemo?</p>
+
+        </div>
+
+        <div class="socialIcons">
+            <a href="#">Twitter</a>
+            <a href="#">Facebook</a>
+            <a href="#">Linkedin</a>
+            <a href="#">Instagram</a>
+        </div>
+
+    </header>
+
+  </div>
+
+
 </body>
 </html>
