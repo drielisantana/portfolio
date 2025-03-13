a1754761c7322b01512d726342823e66e33ffdfa
drielisantana <andrielimartins0308@gmail.com>
2025-03-13 20:38:32 -0300

    aula 13/03

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Portifólio</title>
 
-   
-    
+    <link rel="stylesheet" href="tools/fontawesome/css/all.css"/>
+
     <link rel="stylesheet" href="css/style.css" />
 
 </head>
@@ -25,10 +25,10 @@
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Serviços</a></li>
-                <li><a href="#">Produtos</a></li>
                 <li><a href="#">Portifólio</a></li>
                 <li><a href="#">Sobre</a></li>
                 <li><a href="#">Contato</a></li>
+                <li><a href="#">Restrito</a></li>
             </ul>
 
         </nav>
@@ -42,16 +42,65 @@
             </div>
 
             <div class="socialIcons">
-                <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                <a href="#"><i class="fa-brands fa-facebook"></i></a>
-                <a href="#"><i class="fa-brands fa-linkedin"></i></a>
-                <a href="#"><i class="fa-brands fa-instagram"></i></a>
-            </div>
-        </div> <!-- ========================== -->
-        
+                <a href="#" title="Visite nosso Twitter"><i class="fab fa-twitter"></i></a>
+                <a href="#" title="Visite nosso Facebook"><i class="fab fa-facebook"></i></a>
+                <a href="#" title="Visite nosso Linkedin"><i class="fab fa-linkedin"></i></a>
+                <a href="#" title="Visite nosso Instagram"><i class="fab fa-instagram"></i></a>
+                </div> <!-- ========================== -->
+             
     </header>
+
+
+    <!-- ========================== -->
+ 
+    <main id= "corpo">
+        <section class = "servico">
+
+        <article class="servico">
+            <img src="imagens/service-design.svg" alt="Design"/>
+            <h3 class="title-servicle">
+            <p class="text-servico">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error odit neque sapiente molestiae officia quidem.</p>
+        </article>
+        <article class="servico">
+            <img src="Imagens/service-hosting.svg" alt="Hosting">
+            <h3 class="title-servicle"> Web Hosting </h3>
+            <p class="text-servico">Molestias vitae, beatae animi, porro corporis repellat a, minus unde voluptate in quaerat ipsum ullam.</p>
+        </article>
+        <article class="servico">
+            <img src="Imagens/service-social.svg" alt="Social">
+            <h3 class="title-servicle">
+            <p class="text-servico">Nisi quo hic alias placeat aliquam mollitia suscipit quas ut nostrum, similique voluptates repellendus eos.</p>
+        </article>
+        <article class="servico">
+            <img src="Imagens/service-seo.svg" alt="Seo">
+            <h3 class="title-servicle">
+            <p class="text-servico">In amet, sit velit, doloribus quod necessitatibus cum aspernatur facere harum incidunt, dolore mollitia dicta!</p>
+        </article>
+        <article class="servico">
+            <img src="Imagens/service-cloud.svg" alt="Cloud">
+            <h3 class="title-servicle">
+            <p class="text-servico">Aut fugit quaerat, dolorem necessitatibus porro error, consequatur provident rem, veritatis et eius autem a.</p>
+        </article>
+        <article class="servico">
+            <img src="Imagens/service-secure.svg" alt="Security">
+            <h3 class="title-servicle">
+            <p class="text-servico">Modi perspiciatis doloribus impedit quae eveniet illum ullam ad explicabo! Minus dolore ratione saepe harum?</p>
+        </article>
+
+
+
+     </section>
+
+
+      </section class= "sobre">
+
+
+      </section>
+</main>
+
+
 
 </div>
     
 </body>
-</html>
+</html> 
